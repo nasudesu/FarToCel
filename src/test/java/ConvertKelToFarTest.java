29f3950cf998e675fc30babe394a6927dd0c9b3a
Nasim Zouneibiri <111856757+nasudesu@users.noreply.github.com>
2024-03-01 10:35:02 +0200

    Second

--- src/test/java/ConvertKelToFarTest.java
+++ src/test/java/ConvertKelToFarTest.java
@@ -8,5 +8,6 @@ class ConvertKelToFarTest {
     void kelToFar() {
         ConvertKelToFar convertKelToFar = new ConvertKelToFar();
         assertEquals(32, convertKelToFar.kelvinToFahrenheit(273.15));
+        System.out.println("Test 1 passed");
     }
 }
